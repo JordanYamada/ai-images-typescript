1f552c35b2b05adbb8f3f6cfffec43e5062b6e71
Jordan Yamada <sadaotomokane@gmail.com>
2024-03-04 03:44:59 +0000

    I am completing the request to save an image, but the output is not displaying as an image. may need to use asw s3 bucket

--- src/SaveButton.tsx
+++ src/SaveButton.tsx
@@ -8,23 +8,32 @@ interface SaveButtonProps {
 }
 
 
+
+
 const SaveButton:React.FC<SaveButtonProps>=(props)=>{
 
 
   const handleConvert = async () => {
     try {
+      const url: string = "http://localhost:3001/api/v1/saveImage";
+      // const imageBody:{imageUrl:string} = {imageUrl:props.image}
       // Fetch image data from the URL
-      const response: AxiosResponse<ArrayBuffer> = await axios.get(props.image, { responseType: 'arraybuffer' });
-      const blob = new Blob([response.data], { type: response.headers['content-type'] });
-
-      // Create a File object from the blob
-      const file = new File([blob], 'image.png', { type: 'image/png' });
+      // console.log(props.image);
+      const response: AxiosResponse = await axios.post(url, { params: { image: props.image } });
+      console.log('!!!!!!!!!!!!!response: ',response);
 
 
-      // Create a URL representing the image file
-      const url = URL.createObjectURL(file);
-      console.log(url)
-      props.saveImage(url);
+      // Convert the response data into a Blob
+      const blob = new Blob([response.data], { type: 'image/png' });
+      console.log('!!!!!!!!!!!!!!!blob:', blob)
+      
+      // Create a URL representing the Blob
+      const imageUrl = URL.createObjectURL(blob);
+
+      console.log('!!!!!!!!!!!!!!!!ImageUrl: ',imageUrl);
+      // Call the saveImage function with the converted image URL
+      props.saveImage(imageUrl);
+
     } catch (error) {
       console.error('Error converting image:', error);
     }
