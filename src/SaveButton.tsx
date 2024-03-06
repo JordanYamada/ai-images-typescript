c55bfee2f2361f9591b3b4987c89d17ebe282421
Jordan Yamada <sadaotomokane@gmail.com>
2024-03-06 01:25:30 +0000

    clearing out some console logs

--- src/SaveButton.tsx
+++ src/SaveButton.tsx
@@ -16,23 +16,11 @@ const SaveButton:React.FC<SaveButtonProps>=(props)=>{
   const handleConvert = async () => {
     try {
       const url: string = "http://localhost:3001/api/v1/saveImage";
-      // const imageBody:{imageUrl:string} = {imageUrl:props.image}
-      // Fetch image data from the URL
-      // console.log(props.image);
       const response: AxiosResponse = await axios.post(url, { params: { image: props.image } });
-      console.log('!!!!!!!!!!!!!response: ',response);
 
-
-      // Convert the response data into a Blob
-      // const blob = new Blob([response.data], { type: 'image/png' });
-      // console.log('!!!!!!!!!!!!!!!blob:', blob)
-      
-      // Create a URL representing the Blob
-      // const imageUrl = URL.createObjectURL(blob);
 
       const imageUrl = response.data.url;
 
-      console.log('!!!!!!!!!!!!!!!!ImageUrl: ',imageUrl);
       // Call the saveImage function with the converted image URL
       props.saveImage(imageUrl);
 
